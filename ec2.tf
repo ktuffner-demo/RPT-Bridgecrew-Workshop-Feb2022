41fe27a5fcfd4307a042a79efd8662cd91a904aa
ktuffner-demo <98493117+ktuffner-demo@users.noreply.github.com>
2022-02-16 11:55:35 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,6 @@
-resource "aws_instance" "test" {
-  monitoring    = "true"
-  ami           = "ami-04505e74c0741db8d"
-  instance_type = "c4.large"
-  metadata_options {
-    http_endpoint = "disabled"
-  }
-  ebs_optimized = "true"
-  root_block_device {
-    encrypted = "true"
-  }
+resource "aws_instance" "tuffnerinstance" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
   tags = {
     "Name" = "tuffner instance"
   }
